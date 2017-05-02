6cfb0676c349164cc2cab23e2c08ba9cea11a485
Stan <stann@live.nl>
2017-05-02 12:37:47 +0200

    Added named query

--- src/main/java/com/S63B/domain/Entities/Tracker.java
+++ src/main/java/com/S63B/domain/Entities/Tracker.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Tracker.getTracker", query = "SELECT tracker FROM Tracker AS tracker WHERE id = :trackerID")
+})
 public class Tracker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,12 +25,11 @@ public class Tracker {
         this.polls = new ArrayList<>();
     }
 
-    public Tracker(int id, String serialNumber, String country, Car car, List<Pol> polls) {
-        this.id = id;
+    public Tracker(String serialNumber, String country, Car car) {
         this.serialNumber = serialNumber;
         this.country = country;
         this.car = car;
-        this.polls = polls;
+        this.polls = new ArrayList<>();
     }
 
     //region Getters and setters
@@ -36,41 +38,28 @@ public class Tracker {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getSerialNumber() {
         return serialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
     public Car getCar() {
         return car;
-    }
-
-    public void setCar(Car car) {
-        this.car = car;
     }
 
     public List<Pol> getPolls() {
         return polls;
     }
 
-    public void setPolls(List<Pol> polls) {
-        this.polls = polls;
+    public boolean addPol(Pol pol){
+        return polls.add(pol);
     }
 
+    public boolean removePol(Pol pol){
+        return polls.remove(pol);
+    }
     //endregion
 }
