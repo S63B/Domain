81bc5407d2e5aba30e7d1a27c20d73042898cb5a
binq1000 <t.daniels@student.fontys.nl>
2017-06-06 10:26:34 +0200

    Added constructor and Getters/Setters

--- src/main/java/com/S63B/domain/Entities/Account.java
+++ src/main/java/com/S63B/domain/Entities/Account.java
@@ -16,4 +16,34 @@ public class Account {
 	private String username;
 	@JsonIgnore
 	private String password;
+
+	public Account(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
